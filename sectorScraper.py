33be04fe7e97b07487afc6392640094a3794a210
16kozlowskim <michal.kozlowski2@gmail.com>
2018-02-21 15:42:58 +0000

    this gets the companies in the specific industry
    if it is needed can easily extend so that it includes the price and other details of a company in each sector

---
+++ sectorScraper.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import urllib2, cookielib
+from bs4 import BeautifulSoup
+
+# gets company data by sector in the following format
+# [ticker, name]
+# sector_num:
+# Aerospace & Defense - 2710
+# Alternative Energy - 0580
+# Automobiles & Parts - 3350
+# Banks - 8350
+# Beverages - 3530
+# Chemicals - 1350
+# Construction & Materials - 2350
+# Electricity - 7530
+# Electronic & Electrical - 2730
+# Equity Investment Instruments - 8980
+# Financial Services - 8770
+# Fixed Line Telecom - 6530
+# Food & Drug Retailers - 5330
+# Food Producers - 3570
+# Forestry & Paper - 1730
+# Gas, Water & Multiutilities - 7570
+# General Industrials - 2720
+# General Retailers - 5370
+# Health Care Equipment & Services - 4530
+# Household Goods & Home Construction - 3720
+# Industrial Enginnering - 2750
+# Industrial Metals & Mining - 1750
+# Industrial Transportation - 2770
+# Leisure Goods - 3740
+# Life Insurance - 8570
+# Media - 5550
+# Mining - 1770
+# Mobile Telecommunications - 6570
+# Nonequity Investment Instruments - 8990
+# Nonlife Insurance - 8530
+# Oil & Gas Producers - 0530
+# Oil Equipment & Services - 0570
+# Personal Goods - 3760
+# Pharmaceuticals & Biotechnology - 4570
+# Real Estate Investment & Services - 8630
+# Real Estate Investment Trusts - 8670
+# Software & Computer Sertvices - 9530
+# Support Services - 2790
+# Technology Hardware & Equipment - 9570
+# Tobacco - 3780
+# Travel & Leisure - 5750
+def get_sector_data(sector_num):
+    url_builder = []
+    url_builder.append('http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/constituents-indices.html?index=UKX&industrySector=')
+    url_builder.append(sector_num)
+    url_builder.append('&page=1')
+
+    url = ''.join(url_builder)
+
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+
+    req = urllib2.Request(url, headers=hdr)
+
+    try:
+        page = urllib2.urlopen(req)
+    except urllib2.HTTPError, e:
+        print e.fp.read()
+
+    content = page.read()
+
+    soup = BeautifulSoup(content, 'html.parser')
+
+    table = soup.find('table', attrs={'class' : 'table_dati'})
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+
+    data = []
+
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for index, ele in enumerate(cols) if index < 2]
+        data.append([ele for ele in cols if ele])
+
+    for elem in data:
+        print elem
+
+def main():
+    get_sector_data('1770')
+
+if __name__ == '__main__':
+    main()
