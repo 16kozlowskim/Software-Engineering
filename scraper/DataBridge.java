bd6868669a8f592d769147c3726f7a71afc162f8
16kozlowskim <michal.kozlowski2@gmail.com>
2018-02-24 17:46:59 +0000

    Update DataBridge.java

--- scraper/DataBridge.java
+++ scraper/DataBridge.java
@@ -1,71 +1,152 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 
 public class DataBridge {
 
-  public static ArrayList<String[]> getHistoricalData(String ticker, String interval, String startDate, String endDate) {
-    String cmd = "python historicalScrape.py "+ticker+" "+interval+" "+startDate+" "+endDate;
-		String s = "";
+  /*public static void main(String[] args) throws IOException {
+
+    String[] companyData = DataBridge.getCompanyData("iii");
+
+    for (int i = 0; i < companyData.length; i++) {
+      System.out.println(companyData[i]);
+    }
+
+    HashMap<String, String[]> risersFallers = getRisersFallers(false);
+
+    risersFallers.forEach((k,v) -> System.out.println("key: "+k+" value: "+v[0]+" "+v[1]+" "+v[2]+" "+v[3]));
+
+    HashMap<String, String> companyTicker = fillCompany();
+
+    companyTicker.forEach((k,v) -> System.out.println("key: "+k+" value: "+v));
+
+    String[] historical = getHistoricalData("lse.uk", "m", "20161111");
+
+
+    for (int j = 0; j < 6; j++) {
+      System.out.println(historical[j]);
+    }
+
+    ArrayList<String[]> sector = getSectorData("2720");
+
+    for (int i = 0; i < sector.size(); i++) {
+      System.out.println(sector.size());
+      for (int j = 0; j < sector.get(i).length; j++) {
+        System.out.println(sector.get(i)[j]);
+      }
+    }
+
+
+  }*/
+
+  public static String[] getCompanyData(String ticker) throws IOException {
+    String cmd = "python googScraper.py "+ticker;
+    String s = "";
     String csvSplitBy = "@";
-    ArrayList<String[]> data = new ArrayList<String[]>();
+    String[] data = new String[15];
 
-    try {
-			Process p = Runtime.getRuntime().exec(cmd);
+    Process p = Runtime.getRuntime().exec(cmd);
 
-			BufferedReader stdInput = new BufferedReader(new
-			InputStreamReader(p.getInputStream()));
+		BufferedReader stdInput = new BufferedReader(new
+		InputStreamReader(p.getInputStream()));
 
-
-			while ((s = stdInput.readLine()) != null) {
-        data.add(s.split(csvSplitBy));
-			}
-
-      /*for (int j = 0; j < data.size(); j++) {
-        for (int k = 0; k < 5; k++) {
-          System.out.print(data.get(j)[k]+",");
-        }
-        System.out.println("");
-      }*/
+    int i = 0;
+		while ((s = stdInput.readLine()) != null) {
+      data[i] = s;
+      i++;
 		}
-    catch (IOException e) {
-      System.out.println("exception happened - here's what I know: ");
-		  e.printStackTrace();
-			System.exit(-1);
+
+    return data;
+  }
+
+  public static HashMap<String, String[]> getRisersFallers(Boolean getRisers) throws IOException {
+    String cmd;
+    if (getRisers)
+      cmd = "python risersFallers.py risers";
+    else
+      cmd = "python risersFallers.py fallers";
+
+    String s = "";
+    String csvSplitBy = "@";
+    HashMap<String, String[]> data = new HashMap<String, String[]>();
+    String[] arr;
+
+    Process p = Runtime.getRuntime().exec(cmd);
+
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+
+		while ((s = stdInput.readLine()) != null) {
+      arr = s.split(csvSplitBy);
+      String[] temp = {arr[0], arr[2], arr[3], arr[4]};
+      data.put(arr[1], temp);
+		}
+
+    return data;
+  }
+
+  public static HashMap<String, String> fillCompany() throws IOException {
+    String cmd = "python ftseTickerCompanies.py";
+    String s = "";
+    String csvSplitBy = "@";
+    HashMap<String, String> data = new HashMap<String, String>();
+    String[] arr;
+
+
+    Process p = Runtime.getRuntime().exec(cmd);
+
+		BufferedReader stdInput = new BufferedReader(new
+		InputStreamReader(p.getInputStream()));
+
+
+		while ((s = stdInput.readLine()) != null) {
+      arr = s.split(csvSplitBy);
+      data.put(arr[1], arr[0]);
+		}
+
+    return data;
+  }
+
+  public static String[] getHistoricalData(String ticker, String interval, String date) throws IOException {
+    String cmd = "python historicalScrape.py "+ticker+" "+interval+" "+date+" "+date;
+    String s = "";
+    String csvSplitBy = "@";
+    String[] data = null;
+
+
+    Process p = Runtime.getRuntime().exec(cmd);
+
+		BufferedReader stdInput = new BufferedReader(new
+		InputStreamReader(p.getInputStream()));
+
+    int i = 0;
+		while ((s = stdInput.readLine()) != null) {
+      if (i == 0) {
+        i++;
+        continue;
+      }
+      data = s.split(csvSplitBy);
 		}
 
     return data;
 	}
 
-  public static ArrayList<String[]> getSectorData(String sectorNum) {
+  public static ArrayList<String[]> getSectorData(String sectorNum) throws IOException {
 
     String cmd = "python sectorsScraper.py "+sectorNum;
     String s = "";
     String csvSplitBy = "@";
     ArrayList<String[]> data = new ArrayList<String[]>();
 
-    try {
-      Process p = Runtime.getRuntime().exec(cmd);
 
-      BufferedReader stdInput = new BufferedReader(new
-      InputStreamReader(p.getInputStream()));
+    Process p = Runtime.getRuntime().exec(cmd);
+
+    BufferedReader stdInput = new BufferedReader(new
+    InputStreamReader(p.getInputStream()));
 
 
-      while ((s = stdInput.readLine()) != null) {
-        data.add(s.split(csvSplitBy));
-      }
-
-      /*for (int j = 0; j < data.size(); j++) {
-        for (int k = 0; k < 5; k++) {
-          System.out.print(data.get(j)[k]+",");
-        }
-        System.out.println("");
-      }*/
-    }
-    catch (IOException e) {
-      System.out.println("exception happened - here's what I know: ");
-      e.printStackTrace();
-      System.exit(-1);
+    while ((s = stdInput.readLine()) != null) {
+      data.add(s.split(csvSplitBy));
     }
 
     return data;
